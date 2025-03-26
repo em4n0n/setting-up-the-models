cc14e72fdf52d9576bf7e0970707b0e812c93145
david <dmsim159@gmail.com>
2025-03-26 13:13:13 -0700

    register Menu and Booking models

--- littlelemon/restaurant/admin.py
+++ littlelemon/restaurant/admin.py
@@ -2,4 +2,5 @@ from django.contrib import admin
 from .models import Booking, Menu
 
 # Register your models here.
-admin.site.register(Menu, Booking)
+admin.site.register(Menu)
+admin.site.register(Booking)
