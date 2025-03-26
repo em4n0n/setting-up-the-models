22381de89cc57c899ce765c6c8d7d12b9efcbe26
david <dmsim159@gmail.com>
2025-03-26 13:06:08 -0700

    update models.py and makemigrations

--- littlelemon/restaurant/models.py
+++ littlelemon/restaurant/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Booking(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=200)
+    no_of_guests = models.IntegerField()
+    bookingdate = models.DateField()
+    
+class Menu(models.Model):
+    id = models.IntegerField(primary_key=True)
+    title = models.CharField(max_length=255)
+    price = models.FloatField()
+    inventory = models.IntegerField()
