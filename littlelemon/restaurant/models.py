3d8d7416d9b40acac6dce75c25ec5520bd2015ba
david <dmsim159@gmail.com>
2025-03-26 16:34:54 -0700

    return a string for both classes

--- littlelemon/restaurant/models.py
+++ littlelemon/restaurant/models.py
@@ -7,8 +7,14 @@ class Booking(models.Model):
     no_of_guests = models.IntegerField()
     bookingdate = models.DateField()
     
+    def __str__(self):
+        return self.name
+    
 class Menu(models.Model):
     id = models.IntegerField(primary_key=True)
     title = models.CharField(max_length=255)
     price = models.FloatField()
     inventory = models.IntegerField()
+    
+    def __str__(self):
+        return self.title
